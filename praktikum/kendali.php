6108ab5ea0ba73c67f18546aedeef796a36db7f5
Zudin205 <olannayoan123@gmail.com>
2025-03-13 00:31:30 +0700

    Add files via upload

---
+++ praktikum/kendali.php
@@ -0,0 +1,23 @@
+<?php
+$nilai = 75;
+if ($nilai >= 80) {
+echo "Nilai Anda A";
+} elseif ($nilai >= 70) {
+echo "Nilai Anda B";
+} else {
+echo "Nilai Anda C";
+}
+?>
+<?php
+for ($i = 1; $i <= 5; $i++) {
+echo "Perulangan ke-" . $i . "<br>";
+}
+?>
+Perulangan WHILE
+<?php
+$i = 1;
+while ($i <= 5) {
+echo "Perulangan ke-" . $i . "<br>";
+$i++;
+}
+?>
---
+++ praktikum/sintaks.php
@@ -0,0 +1,8 @@
+<?php
+echo "Selamat datang di PHP";
+?>
+<?php
+$nama = "Budi";
+$umur = 20;
+echo "Nama saya " . $nama . " dan saya berumur " . $umur . " tahun.";
+?>
